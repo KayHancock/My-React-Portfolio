60ff2054331b1e4d1c92561ae0bda4707b3800ce
KayHancock <106179836+KayHancock@users.noreply.github.com>
2022-11-12 03:37:23 -0500

    made some improvements

--- my-react-portfolio/src/App.js
+++ my-react-portfolio/src/App.js
@@ -1,20 +1,32 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App container">
+    <div className="App container d-flex align-items-center text-center">
       <header className="App-header card">
         <div class="card-body">
           <h1>Kayla Hancock</h1>
           <h3>@KC3KGQ</h3>
-          <a href="https://github.com/KayHancock">
+          <a
+            href="https://github.com/KayHancock"
+            className="mb-3 mt-3 pb-3 pt-3 ms-3 ps-3 link-light text-white"
+          >
             Here's a link to my github.
           </a>
-          <a href="https://www.linkedin.com/in/kc3kgq/">
+          <br></br>
+          <a
+            href="https://www.linkedin.com/in/kc3kgq/"
+            className="mb-3 mt-3 pb-3 pt-3 ms-3 ps-3 link-light"
+          >
             Here's a link to my LinkedIn.
           </a>
-          <img src="./image.jpg" alt="graphics design is my passion"></img>
+          <br></br>
+          <br></br>
+          <img
+            src={require("./image.jpg")}
+            alt="graphics design is my passion"
+            className="mb-3 mt-3 pb-3 pt-3"
+          ></img>
         </div>
       </header>
     </div>
